b853adca9b63dba8802380ae61f97362a30bece1
mataoshou <mataoshou@163.com>
2017-09-30 10:15:20 +0800

    addCode

--- src/tool/file/BaseFileUtil.java
+++ src/tool/file/BaseFileUtil.java
@@ -108,7 +108,30 @@ public class BaseFileUtil
 		FileUtils.writeLines(dst, codeType2,
 				FileUtils.readLines(src, codeType1));
 	}
-
+	/**
+	 *删除文件、文件夹及下属子文件或文件夹
+	 *@param file 删除的文件的路径
+	 */
+	public static boolean delete(File file){
+		if(!file.exists()){//验证路径是否存在
+			logger.debug(file.getPath()+"   路径不存在！");
+			return false;
+		}
+		if(!file.isDirectory()){//验证是否是文件夹
+			file.delete();//文件直接删除
+			logger.debug("删除文件" +file.getPath());
+			return true;
+		}
+		//遍历文件夹下的文件或文件夹，进行删除
+		File[] files=file.listFiles();
+		for(File f:files){
+			delete(f);//递归删除
+		}
+		file.delete();//删除文件夹
+		logger.debug("删除文件夹" +file.getPath());
+		return true;
+		
+	}
 	//根据url  获取文件类型
 	public static String getFileType(String url) throws Exception
 	{
---
+++ sweet.sql
@@ -0,0 +1,51 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.6.31 : Database - sweet
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sweet` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sweet`;
+
+/*Table structure for table `sweet_program` */
+
+DROP TABLE IF EXISTS `sweet_program`;
+
+CREATE TABLE `sweet_program` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `programName` varchar(64) DEFAULT NULL COMMENT '栏目名称',
+  `parent` int(10) DEFAULT NULL COMMENT '父级编码',
+  `timeCreate` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sweet_program` */
+
+insert  into `sweet_program`(`id`,`programName`,`parent`,`timeCreate`) values (1,'左侧',2,'2017-09-13 09:51:01');
+
+/*Table structure for table `sweet_tree` */
+
+DROP TABLE IF EXISTS `sweet_tree`;
+
+CREATE TABLE `sweet_tree` (
+  `id` bigint(20) DEFAULT NULL,
+  `code` varchar(128) DEFAULT NULL,
+  `parent` varchar(128) DEFAULT NULL,
+  `name` varchar(156) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sweet_tree` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
